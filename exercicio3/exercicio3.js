ce0bea4cdfd885f913c155faeffbd622616075fc
unknown <glopes.p09@gmail.com>
2022-06-07 13:22:33 -0300

    Exercicio 2

---
+++ exercicio3/exercicio3.js
@@ -0,0 +1,14 @@
+
+/* Crie um programa que determine se um número é impar ou par
+
+Dica do sucesso: todo número par é divisível por 2 */
+
+const numero = 26;
+const numeroPar = numero/2
+
+if(numeroPar ===0){
+    console.log("Esse numero é par")
+} else {
+    console.log("Esse numero é impar")
+}
+
