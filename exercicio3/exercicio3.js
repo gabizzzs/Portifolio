97ff3443f01963a19ec53c217dd92ad116486c69
unknown <glopes.p09@gmail.com>
2022-06-10 08:27:45 -0300

    Exercício 3

--- exercicio3/exercicio3.js
+++ exercicio3/exercicio3.js
@@ -3,10 +3,10 @@
 
 Dica do sucesso: todo número par é divisível por 2 */
 
-const numero = 26;
-const numeroPar = numero/2
+const numero = 5;
 
-if(numeroPar ===0){
+
+if(numero % 2 === 0){
     console.log("Esse numero é par")
 } else {
     console.log("Esse numero é impar")
